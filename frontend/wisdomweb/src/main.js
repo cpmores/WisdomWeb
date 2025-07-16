1c6b690ac4b5f0c406f7a5377ef06e658f7227fc
luyaoWSL <17383720118@163.com>
2025-07-16 16:42:27 +0800

    7.16.16:39

--- frontend/wisdomweb/src/main.js
+++ frontend/wisdomweb/src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import router from './router'
 import $ from 'jquery'
 
+/*
 // 开发环境使用mock数据
 if (process.env.NODE_ENV === 'development') {
   const { worker } = await import('./mocks/browser')
   worker.start()
-}
+}*/
 
 // 全局引入jQuery
 window.$ = window.jQuery = $
